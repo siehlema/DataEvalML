1a5488e114ac2702dfae9a2ef95edd100fe5418b
Martin Siehler <martin.siehler@gmx.de>
2018-11-09 23:40:27 +0100

    added setup.py and __init__.py for keras application

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='senvaleva',
+    py_modules=['senvaleva'],
+    version='0.1',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'tensorflow>=1.8.0',
+        'keras',
+        'sklearn'
+    ],
+    description="Repository for Sensor Value Evaluation using Machine Learning",
+    author="Martin Siehler",
+)
